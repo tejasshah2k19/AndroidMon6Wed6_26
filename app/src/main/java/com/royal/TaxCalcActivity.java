72e74877767e493ab601d84a2101cf8a211e5aa2
Tejas Shah <tejasshah2k19@gmail.com>
2026-05-18 19:08:10 +0530

    Tax calc

--- app/src/main/java/com/royal/TaxCalcActivity.java
+++ app/src/main/java/com/royal/TaxCalcActivity.java
@@ -1,6 +1,10 @@
 package com.royal;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class TaxCalcActivity extends AppCompatActivity {
+
+    EditText edtIncome;
+    EditText edtName;
+    Button btnSubmit;
+    TextView tvResult;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,63 @@ public class TaxCalcActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //bind
+        edtIncome = findViewById(R.id.edtTaxCalcIncome);
+        edtName = findViewById(R.id.edtTaxCalcName);
+        btnSubmit = findViewById(R.id.btnTaxCalcSubmit);
+        tvResult  = findViewById(R.id.tvTaxCalcResult);
+
+
+        //click
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String n = edtName.getText().toString();
+                String inc = edtIncome.getText().toString();
+                boolean isError = false;
+
+                if (n.isBlank()) {
+                    isError = true;
+                    edtName.setError("Please Enter name");
+                }
+                if (inc.isBlank()) {
+                    isError = true;
+                    edtIncome.setError("Please Enter Income");
+                }
+
+
+                if (isError == false) {
+
+                    String name = edtName.getText().toString();
+                    int income = Integer.parseInt(edtIncome.getText().toString());
+                    int taxRate = 0;
+                    //Income Tax Slab (₹)Income Tax RateUp to ₹4,00,000Nil₹4,00,001 to ₹8,00,0005%₹8,00,001 to ₹12,00,00010%₹12,00,001 to ₹16,00,00015%₹16,00,001 to ₹20,00,00020%₹20,00,001 to ₹24,00,00025%Above ₹24,00,00030%
+
+                    if (income <= 400000) {
+                        taxRate = 0;
+                    } else if (income >= 400001 && income <= 800000) {
+                        taxRate = 5;
+                    } else if (income >= 800001 && income <= 1200000) {
+                        taxRate = 10;
+                    } else if (income >= 1200001 && income <= 1600000) {
+                        taxRate = 15;
+                    } else if (income >= 1600001 && income <= 2000000) {
+                        taxRate = 20;
+                    } else if (income >= 2000001 && income <= 2400000) {
+                        taxRate = 25;
+                    } else {
+                        taxRate = 30;
+                    }
+
+
+                    tvResult.setText("Your tax Rate is : " + taxRate + "%");
+
+
+                }
+            }
+        });
+
     }
 }
