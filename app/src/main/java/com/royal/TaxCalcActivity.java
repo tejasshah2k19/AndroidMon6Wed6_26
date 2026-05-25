5851acacc414f61e7ef48689afb83d6988f2de31
Tejas Shah <tejasshah2k19@gmail.com>
2026-05-25 18:47:01 +0530

    intent

--- app/src/main/java/com/royal/TaxCalcActivity.java
+++ app/src/main/java/com/royal/TaxCalcActivity.java
@@ -1,5 +1,6 @@
 package com.royal;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +19,7 @@ public class TaxCalcActivity extends AppCompatActivity {
 
     EditText edtIncome;
     EditText edtName;
-    Button btnSubmit;
+    Button btnNext;
     TextView tvResult;
 
     RadioGroup rgRegime;
@@ -38,13 +39,13 @@ public class TaxCalcActivity extends AppCompatActivity {
         //bind
         edtIncome = findViewById(R.id.edtTaxCalcIncome);
         edtName = findViewById(R.id.edtTaxCalcName);
-        btnSubmit = findViewById(R.id.btnTaxCalcSubmit);
+        btnNext = findViewById(R.id.btnTaxCalcNext);
         tvResult  = findViewById(R.id.tvTaxCalcResult);
         rgRegime = findViewById(R.id.rgTaxCalcRegime);
 
 
         //click
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
+        btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -72,45 +73,53 @@ public class TaxCalcActivity extends AppCompatActivity {
 
                     String name = edtName.getText().toString();
                     int income = Integer.parseInt(edtIncome.getText().toString());
-                    int taxRate = 0;
+//                    int taxRate = 0;
                     //Income Tax Slab (₹)Income Tax RateUp to ₹4,00,000Nil₹4,00,001 to ₹8,00,0005%₹8,00,001 to ₹12,00,00010%₹12,00,001 to ₹16,00,00015%₹16,00,001 to ₹20,00,00020%₹20,00,001 to ₹24,00,00025%Above ₹24,00,00030%
 
 
                      RadioButton rb =  findViewById(rbId);
                      String regime =  rb.getText().toString();
+//
+//                     if(regime.toLowerCase().contains("new")) {
+//
+//                         if (income <= 400000) {
+//                             taxRate = 0;
+//                         } else if (income >= 400001 && income <= 800000) {
+//                             taxRate = 5;
+//                         } else if (income >= 800001 && income <= 1200000) {
+//                             taxRate = 10;
+//                         } else if (income >= 1200001 && income <= 1600000) {
+//                             taxRate = 15;
+//                         } else if (income >= 1600001 && income <= 2000000) {
+//                             taxRate = 20;
+//                         } else if (income >= 2000001 && income <= 2400000) {
+//                             taxRate = 25;
+//                         } else {
+//                             taxRate = 30;
+//                         }
+//                     }else{
+//                         //old
+//                         if(income <= 250000){
+//                             taxRate =0;
+//                         }else if(income <=500000){
+//                            taxRate = 5;
+//                         }else if(income <= 1000000){
+//                             taxRate = 20;
+//                         }else if(income > 1000000){
+//                             taxRate = 30;
+//                         }
+//                     }
+//
+//
+//                    tvResult.setText("Your tax Rate is : " + taxRate + "%");
 
-                     if(regime.toLowerCase().contains("new")) {
 
-                         if (income <= 400000) {
-                             taxRate = 0;
-                         } else if (income >= 400001 && income <= 800000) {
-                             taxRate = 5;
-                         } else if (income >= 800001 && income <= 1200000) {
-                             taxRate = 10;
-                         } else if (income >= 1200001 && income <= 1600000) {
-                             taxRate = 15;
-                         } else if (income >= 1600001 && income <= 2000000) {
-                             taxRate = 20;
-                         } else if (income >= 2000001 && income <= 2400000) {
-                             taxRate = 25;
-                         } else {
-                             taxRate = 30;
-                         }
-                     }else{
-                         //old
-                         if(income <= 250000){
-                             taxRate =0;
-                         }else if(income <=500000){
-                            taxRate = 5;
-                         }else if(income <= 1000000){
-                             taxRate = 20;
-                         }else if(income > 1000000){
-                             taxRate = 30;
-                         }
-                     }
-
-
-                    tvResult.setText("Your tax Rate is : " + taxRate + "%");
+                    //navigate to deduction activity
+                    Intent intent = new Intent(getApplicationContext(), TaxDeductionActivity.class);
+                    intent.putExtra("name",name);
+                    intent.putExtra("income",income);
+                    intent.putExtra("regime",regime);
+                    startActivity(intent);
 
 
                 }
